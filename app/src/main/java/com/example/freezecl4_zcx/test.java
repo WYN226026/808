c18812034e321d70f27029286704c45be179b82a
wyn2260 <2260267412wyn@gmail.com>
2025-04-22 08:56:37 +0800

    test

---
+++ app/src/main/java/com/example/freezecl4_zcx/test.java
@@ -0,0 +1,31 @@
+package com.example.freezecl4_zcx;
+
+public class test {
+    String  name ;
+    String  firstName ;
+    String lastName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
